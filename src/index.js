87101d582e52d4a5dc33434d92dfc8118b429cda
saideep1629 <saideepmanve3@gmail.com>
2025-03-27 15:37:44 +0530

    utils parts done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { app } from './app.js'
 
 import { connectDB } from "./db/index.js";
 
@@ -7,3 +8,12 @@ dotenv.config({
 })
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is ruuning at port ${process.env.PORT}`);
+        
+    })
+})
+.catch( (err) => {
+    console.log("mysql db connection failed !!!", err)
+})
