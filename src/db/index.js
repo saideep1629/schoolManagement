c441f597c0d02bd2493ff223b77445ce79dd10ba
saideep1629 <saideepmanve3@gmail.com>
2025-03-27 14:58:32 +0530

    database setup done

---
+++ src/db/index.js
@@ -0,0 +1,25 @@
+import mysql from "mysql2/promise";
+import { DB_NAME } from "../constant.js";
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 20,
+    queueLimit: 0,
+})
+
+const connectDB = async() => {
+    try {
+        const connection = await pool.getConnection();
+        console.log(`connected successfully to mqsql. Database host is ${process.env.DB_HOST}`);
+        connection.release();
+        
+    } catch (error) {
+        console.log("Mysql connection error", error);
+        process.exit(1);
+    }
+}
+
+export { connectDB, pool }
